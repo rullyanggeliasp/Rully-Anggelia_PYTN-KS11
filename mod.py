0fc4fbbb3f77d47091302259dd72b71d7cf73284
rullyanggeliasp <anggeliasp12@gmail.com>
2023-03-03 09:02:01 +0700

    add mod3.py and README

---
+++ mod.py
@@ -0,0 +1,13 @@
+s ='Hactiv8-PTP Python for Data Science'
+a = [100,200,300]
+
+def foo(arg):
+    print('arg =  ' +arg)
+
+class Foo:
+    pass
+
+if (__name__=='__main__'):
+    print('Hello World')
+    print('Hallo Saya Rully')
+    foo('THIS IS AN ARGUMENT')
---
+++ mod3.py
@@ -0,0 +1,12 @@
+s ='Hactiv8-PTP Python for Data Science'
+a = [100,200,300]
+
+def foo(arg):
+    print('arg =  ' +arg)
+
+class Foo:
+    pass
+
+print('Hello World')
+print('Hallo Saya Rully')
+foo('THIS IS AN ARGUMENT')
